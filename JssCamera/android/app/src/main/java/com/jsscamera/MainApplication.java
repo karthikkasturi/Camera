63ee93c64826e0a9b815a63d2f5b6df3abc7361c
Manoj Kande <manoj.kande@ggktech.local>
2018-05-29 20:16:41 +0530

    adding jssCamera

--- JssCamera/android/app/src/main/java/com/jsscamera/MainApplication.java
+++ JssCamera/android/app/src/main/java/com/jsscamera/MainApplication.java
@@ -1,4 +1,4 @@
-package com.ex1;
+package com.jsscamera;
 
 import android.app.Application;
 
@@ -7,6 +7,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import org.reactnative.camera.RNCameraPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNCameraPackage()
       );
     }
 
--- JssCamera/index.js
+++ JssCamera/index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('JustCamera', () => App);
+AppRegistry.registerComponent('JssCamera', () => App);
